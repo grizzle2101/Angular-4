03d3733365c7cbef00cff4ca3bfa144c78975840
conor.mcgrath <conormcgrath123@gmail.com>
2018-06-21 11:51:38 +0100

    Section 2 - Tutorial 6 - Type Assertions

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,51 +1,10 @@
-//Tutorial 3 - My First TypeScript Application
-function log(message) {
-    console.log(message);
-}
-var message = "Hello World";
-log(message);
-//Tutorial 4 - Declaring Variables:
-//Task 1 - Create Loop with VAR
-//Task 2 - Transpile & Run
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('Finally: ' + i);
-}
-//Task 3 - Create Loop with LET
-//Task 4 - TransPile & Run
-function doMoreThings() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('Finally: ' + i);
-}
-doSomething();
-doMoreThings();
-//Tutorial 5 - Types:
-//Task 1 - Int to String 
-var count = 5;
-count = 'a';
-//Task 2 - Not Assigning a Type.
-var a;
-a = 1;
-a = true;
-a = 'a';
-//Task 3-  - Type Annotations:
-var b;
-var c;
-var d;
-var e; //Any Type
-var f; //Number Array
-var g = [1, true, 'a', false]; //Array of Any Type
-b = true; //Warnings!
-//Task 4 - Enums
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 0] = "Red";
-    Color[Color["Green"] = 1] = "Green";
-    Color[Color["Blue"] = 2] = "Blue";
-})(Color || (Color = {}));
-;
-var backgroundColor = Color.Blue;
+//Task 1 - Demonstrating String Functions
+var message = 'abc';
+var lastChar = message.endsWith('c');
+//Task 2 - Breaking IntelliSense Types:
+var newMessage;
+newMessage = 'abc';
+var finalChar = newMessage.endsWith('c'); //We Lose All String Functions
+//Task 3 - Use Type Assertions to Fix:
+var endsWithC = newMessage.endsWith('c');
+var alternativeWay = newMessage.endsWith('c');
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,60 +1,14 @@
-//Tutorial 3 - My First TypeScript Application
-function log(message)
-{
-    console.log(message);
-}
-var message = "Hello World";
 
-log(message);
+//Task 1 - Demonstrating String Functions
+let message = 'abc';
+let lastChar = message.endsWith('c');
 
-//Tutorial 4 - Declaring Variables:
-//Task 1 - Create Loop with VAR
-//Task 2 - Transpile & Run
 
-function doSomething()
-{
-    for(var i=0; i < 5; i++)
-    {
-        console.log(i);
-    }
-    console.log('Finally: ' + i);
-}
+//Task 2 - Breaking IntelliSense Types:
+let newMessage;
+newMessage = 'abc'
+let finalChar = newMessage.endsWith('c'); //We Lose All String Functions
 
-//Task 3 - Create Loop with LET
-//Task 4 - TransPile & Run
-function doMoreThings()
-{
-    for(let i=0; i < 5; i++)
-    {
-        console.log(i);
-    }
-    console.log('Finally: ' + i);
-}
-
-doSomething();
-doMoreThings();
-
-//Tutorial 5 - Types:
-//Task 1 - Int to String 
-let count = 5;
-count = 'a';
-
-//Task 2 - Not Assigning a Type.
-let a;
-a = 1;
-a = true;
-a = 'a';
-
-//Task 3-  - Type Annotations:
-let b: number;
-let c: boolean;
-let d: string;
-let e: any; //Any Type
-let f:number[]; //Number Array
-let g:any[] = [1, true, 'a', false]; //Array of Any Type
-
-b = true; //Warnings!
-
-//Task 4 - Enums
-enum Color{Red = 0, Green = 1, Blue = 2};
-let backgroundColor = Color.Blue;
+//Task 3 - Use Type Assertions to Fix:
+let endsWithC = (<string>newMessage).endsWith('c');
+let alternativeWay = (newMessage as string).endsWith('c');
