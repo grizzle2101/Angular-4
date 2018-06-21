6b9774c0ea582b3a9471948b0850b068298f5a08
conor.mcgrath <conormcgrath123@gmail.com>
2018-06-21 11:09:40 +0100

    Section 2 - Tutorial 4 - Declaring Variables

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,6 +1,25 @@
-//Task 5 - Writing Simple JavaScript as TypeScript
+//Tutorial 3 - My First TypeScript Application
 function log(message) {
     console.log(message);
 }
 var message = "Hello World";
 log(message);
+//Tutorial 4 - Declaring Variables:
+//Task 1 - Create Loop with VAR
+var number = 1;
+var count = 2;
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+//Task 3 - Create Loop with LET
+function doMoreThings() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+doSomething();
+doMoreThings();
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,4 +1,4 @@
-//Task 5 - Writing Simple JavaScript as TypeScript
+//Tutorial 3 - My First TypeScript Application
 function log(message)
 {
     console.log(message);
@@ -6,3 +6,32 @@ function log(message)
 var message = "Hello World";
 
 log(message);
+
+//Tutorial 4 - Declaring Variables:
+//Task 1 - Create Loop with VAR
+//Task 2 - Transpile & Run
+var number = 1;
+let count = 2; 
+
+function doSomething()
+{
+    for(var i=0; i < 5; i++)
+    {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+
+//Task 3 - Create Loop with LET
+//Task 4 - TransPile & Run
+function doMoreThings()
+{
+    for(let i=0; i < 5; i++)
+    {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+
+doSomething();
+doMoreThings();
