8811dfe6cb8a41c50d983910dceaf3c6104531bb
conor.mcgrath <conormcgrath123@gmail.com>
2018-06-21 10:36:48 +0100

    Section 2 - Tutorial 3 - My First TypeScript Application

---
+++ ts-hello/main.js
@@ -0,0 +1,6 @@
+//Task 5 - Writing Simple JavaScript as TypeScript
+function log(message) {
+    console.log(message);
+}
+var message = "Hello World";
+log(message);
---
+++ ts-hello/main.ts
@@ -0,0 +1,8 @@
+//Task 5 - Writing Simple JavaScript as TypeScript
+function log(message)
+{
+    console.log(message);
+}
+var message = "Hello World";
+
+log(message);
