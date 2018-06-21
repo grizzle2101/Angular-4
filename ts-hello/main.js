33b2c0f53313e55fb64f20fe65543b9314bfb476
conor.mcgrath <conormcgrath123@gmail.com>
2018-06-21 11:33:12 +0100

    Section 2 - Tutorial 5 - Types

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -6,8 +6,7 @@ var message = "Hello World";
 log(message);
 //Tutorial 4 - Declaring Variables:
 //Task 1 - Create Loop with VAR
-var number = 1;
-var count = 2;
+//Task 2 - Transpile & Run
 function doSomething() {
     for (var i = 0; i < 5; i++) {
         console.log(i);
@@ -15,6 +14,7 @@ function doSomething() {
     console.log('Finally: ' + i);
 }
 //Task 3 - Create Loop with LET
+//Task 4 - TransPile & Run
 function doMoreThings() {
     for (var i = 0; i < 5; i++) {
         console.log(i);
@@ -23,3 +23,29 @@ function doMoreThings() {
 }
 doSomething();
 doMoreThings();
+//Tutorial 5 - Types:
+//Task 1 - Int to String 
+var count = 5;
+count = 'a';
+//Task 2 - Not Assigning a Type.
+var a;
+a = 1;
+a = true;
+a = 'a';
+//Task 3-  - Type Annotations:
+var b;
+var c;
+var d;
+var e; //Any Type
+var f; //Number Array
+var g = [1, true, 'a', false]; //Array of Any Type
+b = true; //Warnings!
+//Task 4 - Enums
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Blue;
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -10,8 +10,6 @@ log(message);
 //Tutorial 4 - Declaring Variables:
 //Task 1 - Create Loop with VAR
 //Task 2 - Transpile & Run
-var number = 1;
-let count = 2; 
 
 function doSomething()
 {
@@ -35,3 +33,28 @@ function doMoreThings()
 
 doSomething();
 doMoreThings();
+
+//Tutorial 5 - Types:
+//Task 1 - Int to String 
+let count = 5;
+count = 'a';
+
+//Task 2 - Not Assigning a Type.
+let a;
+a = 1;
+a = true;
+a = 'a';
+
+//Task 3-  - Type Annotations:
+let b: number;
+let c: boolean;
+let d: string;
+let e: any; //Any Type
+let f:number[]; //Number Array
+let g:any[] = [1, true, 'a', false]; //Array of Any Type
+
+b = true; //Warnings!
+
+//Task 4 - Enums
+enum Color{Red = 0, Green = 1, Blue = 2};
+let backgroundColor = Color.Blue;
