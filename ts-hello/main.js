f179fee5e098c3dd32ce4c656f699314c27b287f
conor.mcgrath <conormcgrath123@gmail.com>
2018-06-21 16:05:03 +0100

    Section 2 - Tutorial 14 - Properties

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,15 +1,56 @@
-//Section 2 - Tutorial 13 - Access Modifiers in Construcors
-//Task 1 - Remove instantiation & Assignment of X & Y
-//Task 2 - Apply Access Modifiers on Constructor Directly
+//Section 2 - Tutorial 14 - Properties
+//Task 1 - getX & setX
+/*
+class Point {
+    constructor(private x?: number, private y?: number) {
+    }
+    draw()
+    {
+        console.log('Draw: X - ' + this.x + ', Y -' + this.y);
+    }
+    getX()
+    {
+        return this.x;
+    }
+    setX(value)
+    {
+        if(value < 0)
+            throw new Error('Value Cannot be less than 0.');
+        this.x = value;
+    }
+}
+let point = new Point(1, 2);
+point.draw();
+point.setX(10);
+point.getX();
+point.draw();
+*/
+//Task 2 - Refactor - Use TypeScript Properties
+//TypeScript Conventions - CamelCase for Method & variable names.
+//From the outside X appears as a property, but internally it is really a method.
 var Point = /** @class */ (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
+    function Point(_x, _y) {
+        this._x = _x;
+        this._y = _y;
     }
     Point.prototype.draw = function () {
-        console.log('Draw: X - ' + this.x + ', Y -' + this.y);
+        console.log('Draw: X - ' + this._x + ', Y -' + this._y);
     };
+    Object.defineProperty(Point.prototype, "x", {
+        get: function () {
+            return this._x;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error('Value Cannot be less than 0.');
+            this._x = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return Point;
 }());
 var point = new Point(1, 2);
+point.draw();
+point.x = 10;
 point.draw();
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,6 +1,6 @@
-//Section 2 - Tutorial 13 - Access Modifiers in Construcors
-//Task 1 - Remove instantiation & Assignment of X & Y
-//Task 2 - Apply Access Modifiers on Constructor Directly
+//Section 2 - Tutorial 14 - Properties
+//Task 1 - getX & setX
+/*
 class Point {
     constructor(private x?: number, private y?: number) {
     }
@@ -8,7 +8,49 @@ class Point {
     {
         console.log('Draw: X - ' + this.x + ', Y -' + this.y);
     }
+    getX()
+    {
+        return this.x;
+    }
+    setX(value)
+    {
+        if(value < 0)
+            throw new Error('Value Cannot be less than 0.');
+        this.x = value;
+    }
+}
+let point = new Point(1, 2);
+point.draw();
+point.setX(10);
+point.getX();
+point.draw();
+*/
+
+
+//Task 2 - Refactor - Use TypeScript Properties
+//TypeScript Conventions - CamelCase for Method & variable names.
+//From the outside X appears as a property, but internally it is really a method.
+class Point {
+    constructor(private _x?: number, private _y?: number) {
+    }
+    draw()
+    {
+        console.log('Draw: X - ' + this._x + ', Y -' + this._y);
+    }
+    get x()
+    {
+        return this._x;
+    }
+    set x(value)
+    {
+        if(value < 0)
+            throw new Error('Value Cannot be less than 0.');
+        this._x = value;
+    }
 }
 
 let point = new Point(1, 2);
+
+point.draw();
+point.x = 10;
 point.draw();
