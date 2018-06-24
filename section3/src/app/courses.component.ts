03fcca9b4a5a95b1a777e5df45b9714b6578ce90
Conor McGrath <conormcgrath123@gmail.com>
2018-06-24 20:55:32 +0100

    Section 3 - Tutorial 6 - Directives

--- section3/src/app/courses.component.ts
+++ section3/src/app/courses.component.ts
@@ -1,15 +1,19 @@
-//Task 1 - Create Filer courses.component.ts (notice the naming convention)
-//Task 2 - Create a Class and Module
-//Task 3 - Decorate Class as a Component
-//Task 4 - Register our Component in a Module
-//Task 5 - Add Reference in HTML
+//Section 3 - Tutorial 6 - Directives
+//Task 1 - Return List of Courses
 
 import {Component} from '@angular/core'
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>' //Can Point to HTML page.
+    template: `<h2>{{title}}</h2>
+                <ul>
+                    <li *ngFor="let course of courses">
+                    {{course}}
+                    </li>
+                </ul>` 
 })
 export class CoursesComponent {
+    title = "List of Courses:";
+    courses = ["courseOne", "courseTwo", "courseThree"];
 
 }
