8d800eb6f656aa318180851855033dfe41405d1a
Conor McGrath <conormcgrath123@gmail.com>
2018-06-25 21:42:42 +0100

    Section 4 - Tutorial 4 - Adding BootStrap

--- section3/src/app/courses.component.ts
+++ section3/src/app/courses.component.ts
@@ -1,19 +1,19 @@
-//Section 4 - Tutorial 3 - Attribute Binding
-//Task 1 - Demonstrate String Interpolation V Property Binding
+//Section 4 - Tutorial 5 - Class Binding:
+//Task 1 - Apply Class Binding on Button
+//Task 2 - Add Property to CourseComponent
 import {Component} from '@angular/core'
 import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
     template: `
-                <img [src] ="imageUrl"/>
                 <table>
                     <tr>
                         <td [attr.colspan]="colSpan"></td>
                     </tr>
-                </table>` 
+                </table>
+                <button class="btn btn-primary" [class.active]="isActive">Save</button>` 
 })
 export class CoursesComponent {
-    imageUrl = "https://d1yn1kh78jj1rr.cloudfront.net/previews1/free-sample-stamp_MJl0grOu.jpg";
-    colSpan = 2;
+    isActive = true;
 }
