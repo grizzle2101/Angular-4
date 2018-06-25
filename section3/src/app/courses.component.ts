d65707ab4f55ddef136d703b6b1d1d630807ddd3
Conor McGrath <conormcgrath123@gmail.com>
2018-06-25 18:47:18 +0100

    Section 4 - Tutorial 2 - Property Binding

--- section3/src/app/courses.component.ts
+++ section3/src/app/courses.component.ts
@@ -1,24 +1,21 @@
-//Section 3 - Tutorial 10 - Finals Excercise
-//Implment an Authors Page & Setup
-//Task 1 - Rework HTML
-//Task 2 - Rework Components & Services w Dependency Injection
+//Section 4 - Tutorial 2 - Property Binding
+//Task 1 - Demonstrate String Interpolation V Property Binding
 import {Component} from '@angular/core'
 import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
-    template: `<h2>{{title}}</h2>
-                <ul>
-                    <li *ngFor="let course of courses">
-                    {{course}}
-                    </li>
-                </ul>` 
+    template: ` <h2>{{title}}</h2>
+                <h2>[textContent] = "title"</h2>
+                <img [src] ="imageUrl"/>
+                <img src="{{ imageUrl }}"/>` 
 })
 export class CoursesComponent {
     title = "List of Courses:";
+    imageUrl = "https://d1yn1kh78jj1rr.cloudfront.net/previews1/free-sample-stamp_MJl0grOu.jpg";
     courses;
 
-    //Task 1 - Inject Dependency as Parameter
+
     constructor(service: CoursesService)
     {
         this.courses = service.getCourses();
