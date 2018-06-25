d3d5781c759b6361c9b69871213a59cbebdb91eb
Conor McGrath <conormcgrath123@gmail.com>
2018-06-25 21:48:11 +0100

    Section 4 - Tutorial 5 - Class Binding

--- section3/src/app/courses.component.ts
+++ section3/src/app/courses.component.ts
@@ -1,22 +1,19 @@
-//Section 4 - Tutorial 4 - Adding BootStrap
-//Task 1 - NPM Install bootstrap
-//Task 2 - Add bootstrap to global style.css
-//Task 3 - Use BootStrap Button
+//Section 4 - Tutorial 5 - Class Binding:
+//Task 1 - Apply Class Binding on Button
+//Task 2 - Add Property to CourseComponent
 import {Component} from '@angular/core'
 import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
     template: `
-                
                 <table>
                     <tr>
                         <td [attr.colspan]="colSpan"></td>
                     </tr>
                 </table>
-                <button class="btn btn-primary">Save</button>` 
+                <button class="btn btn-primary" [class.active]="isActive">Save</button>` 
 })
 export class CoursesComponent {
-    imageUrl = "https://d1yn1kh78jj1rr.cloudfront.net/previews1/free-sample-stamp_MJl0grOu.jpg";
-    colSpan = 2;
+    isActive = true;
 }
