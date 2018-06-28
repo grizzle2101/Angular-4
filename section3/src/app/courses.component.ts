1030f74a808e30c7fc88e39f0d52d9d771371704
Conor McGrath <conormcgrath123@gmail.com>
2018-06-28 17:04:36 +0100

    Section 4 - Tutorial 6 - Style Binding

--- section3/src/app/courses.component.ts
+++ section3/src/app/courses.component.ts
@@ -1,18 +1,14 @@
-//Section 4 - Tutorial 5 - Class Binding:
-//Task 1 - Apply Class Binding on Button
-//Task 2 - Add Property to CourseComponent
+//Section 4 - Tutorial 6 - Style Binding
+//Task 1 - Appy Class Binding on Button
+//Task 2 - Add isActive Property to CourseComponent:
+//For more on Style.properties google DOM Style object properties
 import {Component} from '@angular/core'
 import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
     template: `
-                <table>
-                    <tr>
-                        <td [attr.colspan]="colSpan"></td>
-                    </tr>
-                </table>
-                <button class="btn btn-primary" [class.active]="isActive">Save</button>` 
+                <button class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>` 
 })
 export class CoursesComponent {
     isActive = true;
