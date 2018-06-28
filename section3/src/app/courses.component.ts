aec996f96273acf668896f40e2cacd26b561efe7
Conor McGrath <conormcgrath123@gmail.com>
2018-06-28 18:00:51 +0100

    Section 4 - Tutorial 9 - Template Variables

--- section3/src/app/courses.component.ts
+++ section3/src/app/courses.component.ts
@@ -1,20 +1,20 @@
-//Section 4 - Tutorial 8 - Event Filtering
-//Task 1 - Demonstrate Traditional Event Filtering
-//Task 2 - Angular Version
-//Much nicer, no need to do Event Filtering in the Component.
+//Section 4 - Tutorial 9 - Template Variables:
+//How can we get the value from the input field?
+//Task 1 - Get Value through event object
+//Task 2 - Angular Version - template Variables
 import {Component} from '@angular/core'
 import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
     template: `
-                <input (keyup.enter)="onKeyUp()" />` 
+                <input #email (keyup.enter)="onKeyUp(email.value)" />` 
 })
 export class CoursesComponent {
     isActive = true;
     
-    onKeyUp()
+    onKeyUp(email)
     {
-        console.log("Enter was Pressed");        
+        console.log(email);        
     }
 }
