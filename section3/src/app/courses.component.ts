d71d7320443f64264b23522c2ded192b9919949c
Conor McGrath <conormcgrath123@gmail.com>
2018-06-28 17:22:11 +0100

    Section 4 - Tutorial 7 - Event Binding

--- section3/src/app/courses.component.ts
+++ section3/src/app/courses.component.ts
@@ -1,15 +1,30 @@
-//Section 4 - Tutorial 6 - Style Binding
-//Task 1 - Appy Class Binding on Button
-//Task 2 - Add isActive Property to CourseComponent:
-//For more on Style.properties google DOM Style object properties
+//Section 4 - Tutorial 7 - Event Binding
+//Task 1 - Bind Click Event to OnSave Method
+//Tasl 2 - Implment onSave Method.
+//Task 3 - Pass Event Object. 
+//Task 4 - Demonstrate Event Bubbling
+//Task 5 - Stop Event Bubbling with stopPropagation
 import {Component} from '@angular/core'
 import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
     template: `
-                <button class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>` 
+                <div (click)="onDivClick()">
+                <button (click)="onSave($event)">Save</button>
+                </div>` 
 })
 export class CoursesComponent {
     isActive = true;
+    
+    onDivClick()
+    {
+        console.log("Div was clicked Saved");
+    }
+    onSave($event)
+    {
+        $event.stopPropagation();
+        
+        console.log("Data Saved",  $event);
+    }
 }
