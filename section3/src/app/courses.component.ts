270c83aa269cf6e2c473a5348ea1b6d3ef86e696
Conor McGrath <conormcgrath123@gmail.com>
2018-06-29 01:41:21 +0100

    Section 4 - Tutorial 11 - Pipes

--- section3/src/app/courses.component.ts
+++ section3/src/app/courses.component.ts
@@ -1,23 +1,27 @@
-//Section 4 - Tutorial 10 - Two Way Data Binding:
-//In OO, we should be objects, not variables & properties like we have been doing.
-//Task 1 - Use Property Binding to set Email.
-//Task 2 - 2 Way Bind to NGModel
-//Task 3 - Import NgModel
+//Section 4 - Tutorial 11 - Pipes
+//Task 1 - Setup Course Object & Binding in Template
+//Task 2 - 
 import {Component} from '@angular/core'
 import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
     template: `
-                <!--<input [value]="email" (keyup.enter)="email= $event.target.value; onKeyUp()" />-->
-                <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />` 
+                {{course.title | uppercase | lowercase}}<br/>
+                {{course.students | number}}<br/>
+                {{course.rating | number: '1.1-1'}}<br/>
+                {{course.price | currency:'AUD':true:'3.2-2'}}<br/>
+                {{course.releaseDate | date:'shortDate' }}<br/>
+                ` 
 })
 
 export class CoursesComponent {
-    email="me@example.com";
-    
-    onKeyUp(email)
+    course = 
     {
-        console.log(this.email);        
+        title: "The Complete Angular Course",
+        rating: 4.975,
+        students: 47.95,
+        price: 190.95,
+        releaseDate: new Date(2016, 3 ,1)
     }
 }
