0c904dabd03ec06ea8c9e55fff063d37cc046cdf
Conor McGrath <conormcgrath123@gmail.com>
2018-07-01 13:19:21 +0100

    Section 5 - Tutorial 2 - Component API

--- section3/src/app/star/star.component.ts
+++ section3/src/app/star/star.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+//Section 5 - Tutorial 2 - Component API:
+//Task 1 - Return some Data in app.component
+//Task 2 - Attempt to Bind Post data to StartSelected property
+//Problem - Even though both post and Star have the property, its impossible to bind.
+//Solution - We need a Output Property
+
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
