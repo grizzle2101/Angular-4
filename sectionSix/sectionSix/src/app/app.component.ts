04155388bc5ad72a6724efdecb0892f3db9b0b6b
Grizzz <conormcgrath123@gmail.com>
2018-07-22 17:36:34 +0100

    Section 6 - Tutorial 10 - NgStyle

--- sectionSix/sectionSix/src/app/app.component.ts
+++ sectionSix/sectionSix/src/app/app.component.ts
@@ -6,35 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  courses = [
-    {id: 1, name: 'CourseOne'},
-    {id: 2, name: 'CourseTwo'},
-    {id: 3, name: 'CourseThree'}];
-
-  trackCourse(index, course: Course)
-  {
-    return course ? course.id : undefined;
-  }
-
-  onAdd()
-  {
-    this.courses.push({id: 4, name: 'CourseFour'});
-  }
-  onRemove(course)
-  {
-    let index = this.courses.indexOf(course);
-    this.courses.splice(index);
-  }
-  onChange(course : Course)
-  {
-    course.name = 'UPDATED NAME';
-  }
-}
-
-//Using Course Objects for Compile Time Checking.
-export class Course
+export class AppComponent 
 {
-  id: number;
-  name: string;
+  canSave = true;
+
+  onClick()
+  {
+    this.canSave = !this.canSave;
+  }
 }
