198dd582cc4fb144320c93addccc88ce2da760b6
Conor McGrath <conormcgrath123@gmail.com>
2018-07-28 14:18:35 +0100

    Section 7 - Tutorial 4 - NgModel

--- Section-7/Section7/src/app/contact-form/contact-form.component.ts
+++ Section-7/Section7/src/app/contact-form/contact-form.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ContactFormComponent  
+{
+  //Task 3 - Log the ngModel to Console
+  log(x)
+  {
+    console.log(x);
   }
-
 }
