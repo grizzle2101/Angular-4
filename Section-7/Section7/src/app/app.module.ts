fbae57d76d2abdaf4a70ae799d675ed457c08593
Conor McGrath <conormcgrath123@gmail.com>
2018-07-28 13:43:50 +0100

    Section 7 - Tutorial 2 - Building Basic Bootstrap Form

---
+++ Section-7/Section7/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
