e440202225639369e1368775dbc65045adf60656
conor.mcgrath <conormcgrath123@gmail.com>
2018-08-02 16:16:52 +0100

    Section 8 - Tutorial 7 - Async Operations

--- Section-8/Section8/src/app/signup-form/username.validators.ts
+++ Section-8/Section8/src/app/signup-form/username.validators.ts
@@ -1,15 +1,27 @@
 import { AbstractControl, ValidationErrors } from "../../../node_modules/@angular/forms";
 
-//Task 1 - Create Validator Class
 export class UsernameValidators
 {
-    //Task 2 - Create Validator Method
+
     static cannotContainSpace(control: AbstractControl) : ValidationErrors | null
     {
         if((control.value as string).indexOf(' ') >=0)
             return {cannotContainSpace: true};
 
-        return null; //Else Do Nothing.
+        return null; 
+    }
+
+    //Task 1 - Create New Validator
+    static shouldBeUnique(control: AbstractControl) : ValidationErrors | null
+    {
+        //Task 2 - Simulate Asynchonous Operation
+        setTimeout(() => {
+            if(control.value == 'mosh')
+            return {shouldBeUnique: true};
+        return null;
+        }, 2000);
+
+        return null; // Will Always Return Null...
     }
 }
 
