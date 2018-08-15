e484d61d767ed57f3be21ddbe6ff28482d026ef1
conor.mcgrath <conormcgrath123@gmail.com>
2018-08-15 09:38:47 +0100

    Section 9 - Tutorial 11 - Handling Unexpected Errors

--- Section9/src/app/posts/posts.component.ts
+++ Section9/src/app/posts/posts.component.ts
@@ -16,11 +16,15 @@ export class PostsComponent implements OnInit
     
   }
 
+  //Task 1 - Add Error Method to ngOnInit():
   ngOnInit()
   {
     this.service.getPosts()
     .subscribe(response => {
       this.posts = response.json();
+    }, error => {
+      alert('Unpected Error occured.');
+      console.log(error);
     });
   }
 
@@ -33,6 +37,9 @@ export class PostsComponent implements OnInit
     .subscribe(response => {
       post.id = response.json().id;
       this.posts.splice(0, 0, post);
+    }, error => {
+      alert("Unexpected Error occured.");
+      console.log(error);
     });
   }
 
@@ -41,6 +48,9 @@ export class PostsComponent implements OnInit
     this.service.updatePosts(post)
     .subscribe(response => {
       console.log(response.json());
+    }, error => {
+      alert("Unecpted Error occured.");
+      console.log(error);
     });
   }
 
@@ -53,6 +63,9 @@ export class PostsComponent implements OnInit
 
       let index = this.posts.indexOf(post);
       this.posts.splice(index, 1);
+    }, error => {
+      alert("Unexpected Error occured.");
+      console.log(error);
     });
   }
 }
