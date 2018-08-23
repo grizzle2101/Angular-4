9d45b5228c9ca7d67b8ea5af7b32ea9bc0b369b0
conor.mcgrath <conormcgrath123@gmail.com>
2018-08-23 10:13:10 +0100

    Section 9 - Tutorial 19 - Map Operator

--- Section9/src/app/posts/posts.component.ts
+++ Section9/src/app/posts/posts.component.ts
@@ -18,31 +18,30 @@ export class PostsComponent implements OnInit
     
   }
 
+  //Task 2 - Populate Posts Arra
   ngOnInit()
   {
     this.service.getAll()
-    .subscribe(response => {
-      this.posts = response.json();
-    }
-  );
+    .subscribe(posts => this.posts = posts);
   }
 
 
+  //Tak 4 - Final Cleanup
   createPost(input: HTMLInputElement)
   {
     let post: any = {title: input.value};
     input.value = "";
 
    this.service.create(post)
-    .subscribe(response => {
-      post.id = response.json().id;
+    .subscribe(newPost => {
+      post.id = newPost.id;
       this.posts.splice(0, 0, post);
     }, 
     (error: AppError) => {
       if(error instanceof BadData)
       {
         alert("Bad Data!");
-        //this.form.setErrors(error.json);
+        //this.form.setErrors(error.json); //Set Errors on Form
       }
       else throw error; 
     });
@@ -51,19 +50,13 @@ export class PostsComponent implements OnInit
   updatePost(post)
   {
     this.service.update(post)
-    .subscribe(response => 
-      {
-        console.log(response.json());
-      }
-    );
+    .subscribe(updatedPost => console.log(updatedPost));
   }
 
   deletePost(post)
   {
     this.service.delete(post.id)
-    .subscribe(response => {
-      console.log(response.json());
-
+    .subscribe(() => {
       let index = this.posts.indexOf(post);
       this.posts.splice(index, 1);
     }, 
--- Section9/src/app/services/data.service.ts
+++ Section9/src/app/services/data.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http } from '../../../node_modules/@angular/http';
 import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { AppError } from '../common/app.error';
 import { NotFoundError } from '../common/not-found-error';
 import { BadData } from '../common/bad-request-error';
 
 import 'rxjs/Operator/catch/'
 import 'rxjs/add/observable/throw'
+import 'rxjs/Operator/map/'
 
 @Injectable({
   providedIn: 'root'
@@ -19,32 +20,30 @@ export class DataService {
 
   }
 
+  //Task 1 - Map Responce JSON
   getAll()
   {
     let source = this.http.get(this.url);
-    let obj = source.pipe(catchError(this.handleError));
-    return obj;
+    return source.pipe(map(response => response.json()), catchError(this.handleError));
   }
 
+  //Task 3 - Refactor Remaining Methods
   create(resource)
   {
     let source = this.http.post(this.url, JSON.stringify(resource));
-    let obj = source.pipe(catchError(this.handleError));
-    return obj;
+    return source.pipe(map(response => response.json()), catchError(this.handleError));
   }
 
   update(resource)
   {
     let source = this.http.patch(this.url + '/' + resource.id,JSON.stringify({isRead: true}));
-    let obj = source.pipe(catchError(this.handleError));
-    return obj;
+    return source.pipe(map(response => response.json()), catchError(this.handleError));
   }
 
   delete(id: number)
   {
     let source = this.http.delete(this.url + '/' + id);
-    let obj = source.pipe(catchError(this.handleError));
-    return obj;
+    return source.pipe(map(response => response.json()), catchError(this.handleError));
   }
 
 
