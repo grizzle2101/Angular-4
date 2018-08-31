e20585f2e1e487266e55f1c70518d85441cebadb
conor.mcgrath <conormcgrath123@gmail.com>
2018-08-31 14:24:27 +0100

    Section 10 - Tutorial 3 - Configuring Routes

--- Section10/src/app/app.component.ts
+++ Section10/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular app';
+  tweet = { 
+    body: '...',
+    likesCount: 10,
+    isLiked: true
+  }
 }
