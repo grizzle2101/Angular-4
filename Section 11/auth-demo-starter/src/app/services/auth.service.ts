b89d2cd8c753fe603db2753e82eac8374bca9e8b
Conor McGrath <conormcgrath123@gmail.com>
2018-09-08 14:09:21 +0100

    Section 11 - Tutorial 8 - Showing/Hiding Elements

--- Section 11/auth-demo-starter/src/app/services/auth.service.ts
+++ Section 11/auth-demo-starter/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { JwtHelper, tokenNotExpired } from '../../../node_modules/angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -22,15 +23,34 @@ export class AuthService {
       });
   }
 
-  //Task 2 - Implement Logout Method
   logout()
   { 
     console.log("Logging Out");
     localStorage.removeItem('token');
   }
 
+  //Task 2 - Implment IsLoggedIn Method
   isLoggedIn() { 
-    return false;
+    //Task 5 - The Simple Way:
+    return tokenNotExpired();
+
+    /*
+    let jwtHelper = new JwtHelper();
+    let token = localStorage.getItem('token');
+
+    if(!token)
+      return false;
+
+    jwtHelper.decodeToken(token);
+
+    let expirationDate = jwtHelper.getTokenExpirationDate(token);
+    let isExpired = jwtHelper.isTokenExpired(token);
+
+    console.log("Expiration: " , expirationDate);
+    console.log("IsExpired: " , isExpired);
+
+    return !isExpired;
+    */
   }
 }
 
