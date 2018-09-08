9dfbe0642e1bdf89b18e9b83d6ca9a49fe967589
Conor McGrath <conormcgrath123@gmail.com>
2018-09-08 12:26:51 +0100

    Section 11 - Tutorial 6 - Implmenting the Login

--- Section 11/auth-demo-starter/src/app/services/auth.service.ts
+++ Section 11/auth-demo-starter/src/app/services/auth.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
   constructor(private http: Http) {
   }
 
+  //Task 1 -Expore AuthService.Login:
   login(credentials) { 
    return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials));
+      JSON.stringify(credentials))
+      .map( response =>{
+        //console.log(response);
+
+        //Task 2 - Map Responses to True/False
+        let result = response.json();
+        if(result && result.token)
+        {
+          console.log("true");
+          localStorage.setItem('token', result.token);
+          return true
+        }
+        return false;
+      });
   }
 
   logout() { 
