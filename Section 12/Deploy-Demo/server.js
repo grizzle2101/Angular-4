74f5842dfe69fa65f8fcf41ccb9cf9081b1606f4
conor.mcgrath <conormcgrath123@gmail.com>
2018-09-11 15:37:14 +0100

    Section 12 - Tutorial 14 - Deploying to Heroku

--- Section 12/Deploy-Demo/server.js
+++ Section 12/Deploy-Demo/server.js
@@ -1,11 +1,17 @@
 
 const express = require('express');
-const app = express();
+const app = express(); //Create App
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist')); //Serve the WebSite
 
+//In a Real application we would be adding all our API endpoints here.
+//Or in a more complex application, we might have Courses.js, messages.js etc all the service side API components.
+//Using whatever database you have Mongo or SQL.
+
+//Catching all Invalid Routes
 app.all('*', (req, res) => {
   res.status(200).sendFile(__dirname + '/dist/index.html');
 });
 
+//Listen for Requests
 app.listen(process.env.PORT || 8080);
