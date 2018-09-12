6f75eb2aadfb87a88684593a580afa21ed58b7dd
conor.mcgrath <conormcgrath123@gmail.com>
2018-09-12 14:48:30 +0100

    Section 13 - Tutorial 12 - AS Keyword

--- Section 13/firebase-demo/src/app/app.component.ts
+++ Section 13/firebase-demo/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable, Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,10 +15,9 @@ export class AppComponent {
 
   constructor(db: AngularFireDatabase) {
     this.courses$ = db.list('/Courses').valueChanges();
-    //Task 1 - Filter Data for Specific Object:
     this.course$ = db.object('/Courses/3').valueChanges();
 
-    //Task 4 - Create & Display Author Object:
+    //Task 1 - Get Author Data
     this.author$ = db.object('/authors/1').valueChanges();
   }
 }
