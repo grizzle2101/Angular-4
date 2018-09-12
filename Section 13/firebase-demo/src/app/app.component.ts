4d3168500a676e9a777902cde50a0d540452184d
conor.mcgrath <conormcgrath123@gmail.com>
2018-09-12 12:00:45 +0100

    Section 13 - Tutorial 9 - Unsubscribing from Subscription

--- Section 13/firebase-demo/src/app/app.component.ts
+++ Section 13/firebase-demo/src/app/app.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  public courses: any[];
+export class AppComponent implements OnDestroy {
+  courses: any[];
+  subscription: Subscription;
 
-  //Task 1 - Get Courses from Database:
   constructor(db: AngularFireDatabase) {
-    db.list('/Courses').valueChanges().subscribe(courses => {
+    this.subscription = db.list('/Courses').valueChanges().subscribe(courses => {
       this.courses = courses;
       console.log(courses);
     });
+  }
+
+  //Task 1 - Implment ngOnDestroy:
+  ngOnDestroy() {
+    console.log('Gone');
+    this.subscription.unsubscribe();
   }
 }
