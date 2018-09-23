08a1fd8d63ffaa4ea4ea2c4bab1d2a0931b31255
Conor Mc Grath <conormcgrath@Conors-MBP.Home>
2018-09-23 19:41:37 +0100

    Section 14 - Tutorial 6 - Implementing a FadeIn Action

--- Section 14 - Animations/animations-demo/src/app/todos/todos.component.ts
+++ Section 14 - Animations/animations-demo/src/app/todos/todos.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate} from '@angular/animations';
 
+//Task 1 - Add Animations to MetaData:
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
+  animations: [
+    trigger('fade', [ //Create Fade Annotation
+      transition('void => *', [ //On Change from Void to Default
+        style({opacity: 0}), //Styles to Apply
+        animate(2000) //Time & Style for Animated State
+      ])
+    ])
+  ]
 })
 export class TodosComponent {
   items: any[] = [
