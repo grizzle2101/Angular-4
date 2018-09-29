3845773eff8622a97af5b611d13410f49f345014
Conor Mc Grath <conormcgrath@Conors-MBP.Home>
2018-09-29 20:56:04 +0100

    Section 14 - Tutorial 9 - Transitions

--- Section 14 - Animations/animations-demo/src/app/todos/todos.component.ts
+++ Section 14 - Animations/animations-demo/src/app/todos/todos.component.ts
@@ -7,15 +7,13 @@ import { trigger, transition, style, animate, state} from '@angular/animations';
   styleUrls: ['./todos.component.css'],
   animations: [
     trigger('fade', [
-      //Task 1 - Add State
       state('void', style({opacity: 0})),
 
-      transition('void => *', [ 
+      //Task 1 - Combine 2 Transitions:
+      //Task 2 - Use Aliases:
+      transition(':enter, :leave', [ 
         animate(2000)
-      ]),
-      transition('* => void', [
-        animate(2000)
-    ])
+      ])
   ])]
 })
 export class TodosComponent {
