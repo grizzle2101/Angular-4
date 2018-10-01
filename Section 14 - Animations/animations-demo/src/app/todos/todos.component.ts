ad2d70c36c4be3934b5f985a47aeb9f30090f4c7
Conor McGrath <conormcgrath123@gmail.com>
2018-10-01 20:50:23 +0100

    Section 14 - Tutorial 21 - Staggering Animations

--- Section 14 - Animations/animations-demo/src/app/todos/todos.component.ts
+++ Section 14 - Animations/animations-demo/src/app/todos/todos.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { fade, slide, bounceOutLeftAnimation, fadeInAnimation } from '../animations';
-import { trigger, transition, style, animate, useAnimation, query, animateChild, group} from '@angular/animations';
+import { trigger, transition, style, animate, useAnimation, query, animateChild, group, stagger} from '@angular/animations';
 
 @Component({
   selector: 'todos',
@@ -9,19 +9,26 @@ import { trigger, transition, style, animate, useAnimation, query, animateChild,
   animations: [
     trigger('todosAnimation', [
       transition(':enter', [
-        //Task 1 - Group Queries:
-        /* Task 2 - Group Animations Example:
-        group([
-          animate(1000, style({background: 'red'})),
-          animate(2000, style({transform: 'translateY(50px)'}))
-        ])
-        */
         group([
           query('h1', [
             style({transform: 'translateY(-20px)'}),
             animate(1000)
           ]),
-          query('@todoAnimation', animateChild())
+          //Task 1 - Stagger:
+          //query('@todoAnimation', stagger(200, animateChild()))
+          //Task 2 - Staggering useAnimation:
+          //query('@todoAnimation', stagger(200, useAnimation(fadeInAnimation)))
+          //Task 3 - Stagger Function:
+          /*
+          query('.list-group-item', 
+          stagger(200, [
+            style({transform: 'translateX(-20px)', opacity: 0}),
+            animate(1000)
+          ]))
+          */
+         //Task 4 - Final Version
+         query('@todoAnimation', stagger(200, animateChild()))
+          
         ])
       ])
     ]),
