fe86f4339c5c7446a72d4ef76ffd126589d3579e
Conor Mc Grath <conormcgrath@Conors-MBP.Home>
2018-10-07 00:26:07 +0100

    Section 14 - Tutorial 22 - Working with Custom States

--- Section 14 - Animations/animations-demo/src/app/zippy/zippy.component.ts
+++ Section 14 - Animations/animations-demo/src/app/zippy/zippy.component.ts
@@ -1,9 +1,39 @@
 import { Component, Input } from '@angular/core';
+import { trigger, transition, style, animate, useAnimation, query, animateChild, group, stagger, state} from '@angular/animations';
 
+
+//Task 2 - Implement expandCollapse Trigger
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css'],
+  animations: [
+    trigger('expandCollapse', [
+      //Task 3 - Create Custom States
+      //Task 6 - Fix Styleing Issues:
+      state('collapsed', style({
+        height: 0,
+        paddingTop: 0,
+        paddingBottom: 0,
+        overflow: 'hidden',
+        backgroundColor: 'red'
+      })),
+      /*
+      state('expanded',  style({
+        height: '*', //* Means Angular with compute @ runtime.
+        padding: '*',
+        overflow: 'auto'
+      })),
+      */
+      transition('collapsed => expanded', [
+        animate('300ms ease-out')
+      ]),
+      //Task 5 - Implment expanded => collapsed Transition:
+      transition('expanded => collapsed', [
+        animate('300ms ease-in')
+      ])
+    ])
+  ]
 })
 export class ZippyComponent  {
   @Input('title') title: string;
