12c4e09cfe7278ffeffdd7b3d9e8e1afc5a68601
Conor McGrath <conormcgrath123@gmail.com>
2018-10-28 15:23:27 +0000

    Section 15 - Tutorial 9 - DatePicker

--- Section 15 - Angular Material/material-demo/src/app/app.module.ts
+++ Section 15 - Angular Material/material-demo/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import  {BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatCheckboxModule, MatRadioModule, MatSelectModule, MatInputModule } from '@angular/material';
+//Task 1 - Import DataPicker Module:
+import {MatDatepickerModule, MatNativeDateModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -19,9 +21,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatSelectModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
-  providers: [],
+  providers: [MatDatepickerModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  //Task 4 - Limit Date Selection:
+  //Create Min & Max Dates in Component.
+  minDate = new Date(2017, 1, 1);
+  maxDate = new Date(2017, 2, 2);
+
+  
+ }
