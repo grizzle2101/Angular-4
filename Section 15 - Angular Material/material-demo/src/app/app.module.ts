25be4cea3f730dd107250227a98f55a1b2da9816
Conor McGrath <conormcgrath123@gmail.com>
2018-10-28 17:43:07 +0000

    Section 15 - Tutorial 11 - Buttons

--- Section 15 - Angular Material/material-demo/src/app/app.module.ts
+++ Section 15 - Angular Material/material-demo/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 
 import  {BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatCheckboxModule, MatRadioModule, MatSelectModule, MatInputModule,MatDatepickerModule, MatNativeDateModule } from '@angular/material';
-//Task 2 - Import Icons Module:
-import { MatIconModule } from '@angular/material';
+import { MatCheckboxModule, MatRadioModule, MatSelectModule, MatInputModule,MatDatepickerModule, MatNativeDateModule, MatIconModule } from '@angular/material';
+//Task 1 - Import Buttons Module:
+import { MatButtonModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -24,7 +24,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [MatDatepickerModule],
   bootstrap: [AppComponent]
