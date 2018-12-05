58f9b94023fa313190062097300bc0d942f09489
Conor McGrath <conormcgrath123@gmail.com>
2018-12-05 11:59:56 +0000

    Section 17 - Tutorial 6 - Setup & Teardown

--- Section 17 - Unit Testing/testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
+++ Section 17 - Unit Testing/testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,33 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent} from './vote.component';
 
-describe('VoteComponent', () => {
-  it('', () => {
+describe('Vote', () => {
+  let component: VoteComponent;
+
+  //Task 2 - Use Setup & YearDown
+  beforeEach(() => {
+    component = new VoteComponent();
+  })
+
+
+  afterEach(() => {
   });
 
-  it('', () => {
-  });
-});
+  //Task 1 - Setup Test Cases
+  //Test Case 1:
+  it('Should Increment VoteCount', () => {
+    //Act
+    component.upVote();
+
+    //Assert
+    expect(component.totalVotes).toBe(1);
+  })
+
+  //Test Case 2:
+  it('Should Decrement VoteCount', () => {
+    //Act
+    component.downVote();
+
+    //Assert
+    expect(component.totalVotes).toBe(-1);
+  })
+})
