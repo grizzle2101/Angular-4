006779512915b7234d046dae4d44ee7205196a79
Conor McGrath <conormcgrath123@gmail.com>
2018-12-05 13:33:48 +0000

    Section 17 - Tutorial 9 - Working with Spies

--- Section 17 - Unit Testing/testing-demo/src/app/06-services/todos.component.spec.ts
+++ Section 17 - Unit Testing/testing-demo/src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,43 @@
 import { TodosComponent } from './todos.component'; 
 import { TodoService } from './todo.service'; 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+
+    //Passing Mock
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  //Test Case 1:
+  it('Should set Todos to items returned from Server', () => {
+    let todos = [1, 2, 3];
+    //Arrange
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return Observable.from([todos]);
+    });
+
+    //Act
+    component.ngOnInit();
+
+    //Assert
+    expect(component.todos).toBe(todos);
   });
 });
+
+
+/* We can Implment our Spy like so, but it really doesn't provide any value, just noise into our code.
+    We are only testing that NgOnInit populate the list, we don't care with what.
+spyOn(service, 'getTodos').and.callFake(() => {
+      return Observable.from([[
+        {id: 1, title: 'One'},
+        {id: 2, title: 'Two'},
+        {id: 3, title: 'Three'},
+      ]]);
+    });
+*/
