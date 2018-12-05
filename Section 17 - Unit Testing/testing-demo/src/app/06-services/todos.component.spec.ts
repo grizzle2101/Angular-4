e7ea72eb828c7a70577f47ec5ff8f760ffe7437f
Conor McGrath <conormcgrath123@gmail.com>
2018-12-05 16:06:25 +0000

    Section 17 - Tutorial 10 - Interaction Testing

--- Section 17 - Unit Testing/testing-demo/src/app/06-services/todos.component.spec.ts
+++ Section 17 - Unit Testing/testing-demo/src/app/06-services/todos.component.spec.ts
@@ -2,6 +2,9 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service'; 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
+
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -14,10 +17,10 @@ describe('TodosComponent', () => {
     component = new TodosComponent(service);
   });
 
-  //Test Case 1:
+  //Test Case 1 - NgOnInit
   it('Should set Todos to items returned from Server', () => {
-    let todos = [1, 2, 3];
     //Arrange
+    let todos = [1, 2, 3];
     spyOn(service, 'getTodos').and.callFake(() => {
       return Observable.from([todos]);
     });
@@ -28,16 +31,52 @@ describe('TodosComponent', () => {
     //Assert
     expect(component.todos).toBe(todos);
   });
-});
 
+  //Task 1 - Implment Add Tests:
+  //-ToDo is sent to Service
+  //-After Add, the DOM is Updated.
+  //-Failing the Add, an Error is returned.
 
-/* We can Implment our Spy like so, but it really doesn't provide any value, just noise into our code.
-    We are only testing that NgOnInit populate the list, we don't care with what.
-spyOn(service, 'getTodos').and.callFake(() => {
-      return Observable.from([[
-        {id: 1, title: 'One'},
-        {id: 2, title: 'Two'},
-        {id: 3, title: 'Three'},
-      ]]);
+  //Scenario 1 - Verify it makes Server Call
+  it('Should Sent Correct ToDo item to Server', () => {
+    //Arrange
+    let spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.empty();
     });
-*/
+
+    //Act
+    component.add();
+
+    //Assert
+    //We just need to ensure the Spy was called the Act part is done.
+    expect(spy).toHaveBeenCalled();
+  });
+
+
+    //Scenario 2 - Verify it adds the Item returned from server, into DOM.
+    it('Should Add Item returned from Server into the Todo List.', () => {
+      //Arrange
+      let todo = {id: 1};
+      let spy = spyOn(service, 'add').and.returnValue(Observable.from([todo]));
+
+      //Act
+      component.add();
+  
+      //Assert
+      expect(component.todos.length).toBe(1);
+    });
+
+     //Scenario 3 - Verify on Failure, we put that error in the Message property.
+     it('Should update the message property on failure from the service.', () => {
+      //Arrange
+      let error = 'error from the server';
+      let spy = spyOn(service, 'add').and.returnValue(Observable.throw(error));
+
+      //Act
+      component.add();
+  
+      //Assert
+      expect(component.message).toBe(error);
+    });
+
+});
