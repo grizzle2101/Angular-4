efa562e07c6084992577bdd43d17cf8f15f1544d
Conor McGrath <conormcgrath123@gmail.com>
2018-12-05 12:37:52 +0000

    Section 17 - Tutorial 7 - Working with Forms

--- Section 17 - Unit Testing/testing-demo/src/app/04-forms/todo-form.component.spec.ts
+++ Section 17 - Unit Testing/testing-demo/src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,32 @@
 import { TodoFormComponent } from './todo-form.component'; 
+import { FormBuilder } from '@angular/forms';
 
 describe('TodoFormComponent', () => {
   var component: TodoFormComponent; 
 
+  //Task 1 - Setup
+  //Import Instance of FormBuilder Class
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
+  //Task 2 - Defint Test Cases
+  //Test Case 1 - Should create 2 form control objecs Name & Email
+  it('Should create 2 form control objects', () => {
+    //Assert
+    expect(component.form.contains('name')).toBeTruthy(); //Expect Name in Form
+    expect(component.form.contains('email')).toBe(true); //Expect Email in Form
   });
 
-  it('', () => {
+  //Test Case 2 - Test the Validator:
+  it('Should make the form control name Required', () => {
+    //Arrange
+    let control = component.form.get('name');
+
+    //Act
+    control.setValue('');
+
+    //Assert
+    expect(control.valid).toBeFalsy();
   });
 });
