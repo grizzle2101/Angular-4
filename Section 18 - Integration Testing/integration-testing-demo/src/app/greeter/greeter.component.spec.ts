1262bf035ffee8546826b4fd8fcb4971ad64a018
Conor McGrath <conormcgrath123@gmail.com>
2018-12-10 12:19:31 +0000

    Section 18 - Tutorial 3 - Generating the Setup Code

---
+++ Section 18 - Integration Testing/integration-testing-demo/src/app/greeter/greeter.component.spec.ts
@@ -0,0 +1,27 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { GreeterComponent } from './greeter.component';
+
+describe('GreeterComponent', () => {
+  let component: GreeterComponent;
+  let fixture: ComponentFixture<GreeterComponent>;
+
+
+  //Create Component
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GreeterComponent ]
+    });
+
+    fixture = TestBed.createComponent(GreeterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
