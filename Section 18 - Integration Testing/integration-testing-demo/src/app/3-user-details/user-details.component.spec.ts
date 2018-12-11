cabdbe01856e199528a598f22f4fef5dd1c50bb5
Conor McGrath <conormcgrath123@gmail.com>
2018-12-11 18:55:49 +0000

    Section 18 - Tutorial 10 - Dealing with Route Params

--- Section 18 - Integration Testing/integration-testing-demo/src/app/3-user-details/user-details.component.spec.ts
+++ Section 18 - Integration Testing/integration-testing-demo/src/app/3-user-details/user-details.component.spec.ts
@@ -5,15 +5,25 @@ import { DebugElement } from '@angular/core';
 
 import { UserDetailsComponent } from './user-details.component';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 class RouterStub {
   navigate(params) {
   }
 }
 
+//Task 2 - Set Route Params
 class ActivatedRouteStub {
-  params: Observable<any> = Observable.empty();
+  private subject = new Subject();
+
+  //Add Value to Subject
+  push(value) {
+    this.subject.next(value);
+  }
+  //Get Subject as Observable
+  get params() {
+    return this.subject.asObservable();
+  }
 }
 
 
@@ -39,7 +49,7 @@ describe('UserDetailsComponent', () => {
     fixture.detectChanges();
   });
 
-  //Task 1 - Write a Test to Ensure we are reriected onSave:
+  //Test 1 - Ensure we are reriected onSave:
   it('Should redirect User to the Users page after saving', () => {
     //Arrange
     let router = TestBed.get(Router);
@@ -50,5 +60,20 @@ describe('UserDetailsComponent', () => {
 
     //Assert
     expect(spy).toHaveBeenCalledWith(['users']); //Users Page
+  });
+
+  
+  //Test 2 - Should navigate to page not found if user == 0
+  it('Should redirect User to the page not found, when an invalid user id is passed', () => {
+    //Arrange
+    let router = TestBed.get(Router);
+    let spy = spyOn(router, 'navigate');
+
+    //Act
+    let route: ActivatedRouteStub = TestBed.get(ActivatedRoute);
+    route.push({id: 0});
+
+    //Assert
+    expect(spy).toHaveBeenCalledWith(['not-found']);
   });
 });
