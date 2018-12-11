995d9529c8d33284d2eaf0693e1be917acd3a128
Conor McGrath <conormcgrath123@gmail.com>
2018-12-11 00:08:22 +0000

    Section 18 - Tutorial 6 - Providing Dependencies

--- Section 18 - Integration Testing/integration-testing-demo/src/app/2-todos/todos.component.spec.ts
+++ Section 18 - Integration Testing/integration-testing-demo/src/app/2-todos/todos.component.spec.ts
@@ -4,21 +4,22 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { TodosComponent } from './todos.component';
+//Add Imports
+import {TodoService} from './todo.service';
+import {HttpModule} from '@angular/http';
 
-//NOTE: I've deliberately excluded this suite from running
-// because the test will fail. This is because we have not 
-// provided the TodoService as a dependency to TodosComponent. 
-// 
-// When you get to Lecture 6 (Providing Dependencies), be sure
-// to remove "x" from "xdescribe" below. 
 
-xdescribe('TodosComponent', () => {
+describe('TodosComponent', () => {
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
 
+  //Task 1 - Add Dependency:
+  //Task 2 - Import Http Module:
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodosComponent ]
+      imports: [HttpModule],
+      declarations: [ TodosComponent ],
+      providers: [TodoService]
     })
     .compileComponents();
   }));
