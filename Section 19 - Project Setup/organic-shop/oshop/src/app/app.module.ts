de8da027d358656d011649bbc6f2c567fbde3ac5
Conor McGrath <conormcgrath123@gmail.com>
2018-12-20 16:11:36 +0000

    Section 19 - Tutorial 10 - Fixing a Few Minor Issues

--- Section 19 - Project Setup/organic-shop/oshop/src/app/app.module.ts
+++ Section 19 - Project Setup/organic-shop/oshop/src/app/app.module.ts
@@ -48,9 +48,10 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
       {path:'shopping-cart', component: ShoppingCartComponent},
       {path:'check-out', component: CheckOutComponent},
       {path:'order-success', component: OrderSuccessComponent},
+      {path:'my/orders', component: MyOrdersComponent},
       {path:'login', component: LoginComponent},
       {path:'admin/products', component: AdminProductsComponent},
-      {path:'admin/orders', component: AdminOrdersComponent}
+      {path:'admin/orders', component: AdminOrdersComponent},
   ])
   ],
   providers: [],
