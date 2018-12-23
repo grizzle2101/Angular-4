0937ec75ef2856b6b336d32202ce9a7b84d2e36b
Conor McGrath <conormcgrath123@gmail.com>
2018-12-23 17:04:14 +0000

    Section 20 - Tutorial 2 - Implementing Google Login

---
+++ Section 20 - Project Authentication & Authorization/oshop/src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {environment} from './../environments/environment';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+
+import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { LoginComponent } from './login/login.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import {RouterModule} from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    LoginComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      {path:'', component: HomeComponent},
+      {path:'my/orders', component: MyOrdersComponent},
+      {path:'products', component: ProductsComponent},
+      {path:'shopping-cart', component: ShoppingCartComponent},
+      {path:'check-out', component: CheckOutComponent},
+      {path:'order-success', component: OrderSuccessComponent},
+      {path:'login', component: LoginComponent},
+      {path:'admin/products', component: AdminProductsComponent},
+      {path:'admin/orders', component: AdminOrdersComponent},
+  ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Section 20 - Project Authentication & Authorization/oshop/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private afAuth: AngularFireAuth) {}
+
+
+  login()
+  {
+    console.log("Login");
+    //Will Redirect to OAuth provider:
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+}
