b8cbeb15791f9026d72522940a223fbf141f6b72
Conor McGrath <conormcgrath123@gmail.com>
2018-12-23 21:31:19 +0000

    Section 20 - Tutorial 4 - Displaying the Current User

--- Section 20 - Project Authentication & Authorization/oshop/src/app/bs-navbar/bs-navbar.component.ts
+++ Section 20 - Project Authentication & Authorization/oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'bs-navbar',
@@ -7,9 +8,11 @@ import { AngularFireAuth } from 'angularfire2/auth';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
+  //Add User
+  user: firebase.User;
 
   constructor(private afAuth: AngularFireAuth) { 
-    afAuth.authState.subscribe( x => console.log("Status: ", x));
+    afAuth.authState.subscribe( user => this.user = user);
   }
 
   logout() {
