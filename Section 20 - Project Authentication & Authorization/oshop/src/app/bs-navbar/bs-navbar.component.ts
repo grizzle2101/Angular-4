a2108cebaee5aa8262000404943932b64e027b3a
Conor McGrath <conormcgrath123@gmail.com>
2018-12-24 00:31:03 +0000

    Section 20 - Tutorial 5 - Using the Async Pipe

--- Section 20 - Project Authentication & Authorization/oshop/src/app/bs-navbar/bs-navbar.component.ts
+++ Section 20 - Project Authentication & Authorization/oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar',
@@ -9,10 +10,10 @@ import * as firebase from 'firebase';
 })
 export class BsNavbarComponent {
   //Add User
-  user: firebase.User;
+  user$: Observable<firebase.User>
 
   constructor(private afAuth: AngularFireAuth) { 
-    afAuth.authState.subscribe( user => this.user = user);
+    this.user$ = afAuth.authState;
   }
 
   logout() {
