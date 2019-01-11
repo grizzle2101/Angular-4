f43df0f563bc0aa3097410d15bb3cd0a2adcced5
Conor McGrath <conormcgrath123@gmail.com>
2019-01-11 01:34:45 +0000

    Section 21 - Tutorial 7 - Adding a Bootstrap Card

--- Section 21 - Project Product Management/oshop/src/app/admin/product-form/product-form.component.ts
+++ Section 21 - Project Product Management/oshop/src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -10,15 +11,19 @@ import { ProductService } from 'src/app/product.service';
 export class ProductFormComponent implements OnInit {
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService, 
+    private productService: ProductService) {
     this.categories$ = categoryService.getCategories().valueChanges();
-    console.log("Data: ", this.categories$);
    }
 
   ngOnInit() {}
 
+  //Task 4 - Redirect On Save:
   save(product) {
     console.log("Product: ", product);
     this.productService.createProduct(product);
+    this.router.navigate(['admin/products']);
   }
 }
