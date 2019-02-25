20f0e825921ec300c46d03209f3d47fc8f07bbb5
Conor McGrath <conormcgrath123@gmail.com>
2019-02-25 11:36:33 +0000

    Section 24 - Tutorial 8 - Clearing the Shopping Cart

--- Section 24 - Check Out Module/oshop/src/app/order.service.ts
+++ Section 24 - Check Out Module/oshop/src/app/order.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { ShoppingCartService } from './shopping-cart.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private shoppingCart: ShoppingCartService) { }
 
-  storeOrder(order) {
-     return this.db.list('/orders').push(order);
+  async placeOrder(order) {
+     let result = await this.db.list('/orders').push(order);
+     this.shoppingCart.clearCart();
+     return result;
   }
 }
