4bd84818b6da513d5950ec551631ea8368c49938
Conor McGrath <conormcgrath123@gmail.com>
2019-03-01 16:22:04 +0000

    Section 25 - Tutorial 5 - Creating the Shared Module

---
+++ Section 25 - Modularization & Final Improvements/oshop/src/app/shared/shared.module.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductQuantityComponent } from './components/product-quantity/product-quantity.component';
+import { AuthGuard } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
+import { CategoryService } from './services/category.service';
+import { OrderService } from './services/order.service';
+import { ProductService } from './services/product.service';
+import { ShoppingCartService } from './services/shopping-cart.service';
+import { UserService } from './services/user.service';
+
+
+@NgModule({
+  declarations: [
+    ProductCardComponent,
+    ProductQuantityComponent
+  ],
+  exports: [
+    //Expose Components outside of Shared.
+    ProductCardComponent,
+    ProductQuantityComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    AuthService,
+    AuthGuard,
+    UserService,
+    CategoryService,
+    ProductService,
+    ShoppingCartService,
+    OrderService
+  ]
+})
+
+export class SharedModule { }
